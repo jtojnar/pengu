8e7b8bc2097b9ee125afdaedc3faebc1bc244fdd
Jan Tojnar <jtojnar@gmail.com>
2014-04-19 03:40:57 +0200

    added persistence using pgsql

--- README.md
+++ README.md
@@ -3,6 +3,18 @@ pengu
 
 **pengu** is lightweight web chat built with HTML5 resembling Club Penguin.
 
-Can be started with `foreman start`, defaults to port 5000.
+1. Create following table in PostgreSQL database and set `DATABASE_URL` to correct connection string.
+
+```
+CREATE TABLE "penguin" (
+	"name" character varying NOT NULL PRIMARY KEY,
+	"closet" json DEFAULT '[]' NOT NULL,
+	"clothing" json DEFAULT '[]' NOT NULL,
+	"registered" timestamptz DEFAULT current_timestamp NOT NULL
+);
+```
+
+2. Run `npm install`
+3. Start with `foreman start`, defaults to port 5000 (configurable via `POST` env variable).
 
 Licensed under MIT.
--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 "use strict";
 
 process.title = 'pengu';
+var _dbUri = process.env.DATABASE_URL;
 
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var express = require('express');
+var pg = require('pg');
 var poly = require('./poly');
 var Point = poly.Point;
 var Polygon = poly.Polygon;
@@ -90,7 +92,6 @@ function getTarget(room, line) {
 
 var clients = [];
 var players = {};
-var registered = {};
 var rooms = JSON.parse(require('fs').readFileSync(__dirname + '/content/world/map.json', 'utf8'), function (key, value) {
 	var type;
 	if(value && typeof value === 'object') {
@@ -102,115 +103,162 @@ var rooms = JSON.parse(require('fs').readFileSync(__dirname + '/content/world/ma
 	return value;
 });
 
-
-wsServer.on('request', function(request) {
-	if (!originIsAllowed(request.origin)) {
-		request.reject();
-		console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-		return;
+var dbEnabled = true;
+pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
+	if(err) {
+		pgdone();
+		console.error('Couldn\'t connect to database, data aren\'t persitent', err);
+		dbEnabled = false;
 	}
+	var registered = {};
+	if(dbEnabled) {
+		pgclient.query('select * from "penguin"', getPenguinsFromDb);
+	} else {
+		getPenguinsFromDb(null, null);
+	}
+	function getPenguinsFromDb(err, result) {
+		if(!err) {
+			if(result) {
+				for(var i = 0; i < result.rows.length; i++) {
+					registered[result.rows[i].name] = result.rows[i];
+				}
+			}
+		} else {
+			console.error('Couldn\'nt read penguins from db', err);
+			dbEnabled = false;
+		}
+	
+		wsServer.on('request', function(request) {
+			if (!originIsAllowed(request.origin)) {
+				request.reject();
+				console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+				return;
+			}
 
-	var connection = request.accept(null, request.origin);
-	clients.push(connection);
-	console.log((new Date()) + ' Connection accepted.');
-	connection.on('message', function(message) {
-		if(message.type === 'utf8') {
-			try {
-				var json = JSON.parse(message.utf8Data);
-				console.log(json);
-				if(json.type == 'init' && connection.name == null) {
-					var name = connection.name = json.name;
-					connection.sendUTF(JSON.stringify({type: 'sync', name: name, data: players}));
-					if(!registered.hasOwnProperty(name)) {
-						registered[name] = {clothing: [], closet: [], registered: (new Date).toUTCString()};
-					}
-					players[name] = registered[name];
-					players[name].x = 550;
-					players[name].y = 500;
-					players[name].room = 'plaza';
+			var connection = request.accept(null, request.origin);
+			clients.push(connection);
+			console.log((new Date()) + ' Connection accepted.');
+			connection.on('message', function(message) {
+				if(message.type === 'utf8') {
+					try {
+						var json = JSON.parse(message.utf8Data);
+						console.log(json);
+						if(json.type == 'init' && connection.name == null) {
+							var name = connection.name = json.name;
+							connection.sendUTF(JSON.stringify({type: 'sync', name: name, data: players}));
+							if(!registered.hasOwnProperty(name)) {
+								registered[name] = {clothing: [], closet: [], registered: (new Date).toUTCString()};
+								if(dbEnabled) {
+									pgclient.query('insert into "penguin"("name", "closet", "clothing", "registered") VALUES($1, $2, $3, $4)', [name, JSON.stringify(registered[name].closet), JSON.stringify(registered[name].clothing), registered[name].registered], function insertPenguinToDb(err) {
+										if(err) {
+											console.log(err);
+										}
+									});
+								}
+							}
+							players[name] = registered[name];
+							players[name].x = 550;
+							players[name].y = 500;
+							players[name].room = 'plaza';
 
-					console.info('Initial handshake with ' + name);
-					for(var i=0; i < clients.length; i++) {
-						clients[i].sendUTF(JSON.stringify({type: 'enter', name: name, room: players[name].room, x: players[name].x, y: players[name].y, clothing: players[name].clothing}));
-					}
-					connection.sendUTF(JSON.stringify({type: 'syncCloset', closet: players[name].closet}));
-				} else if(json.type == 'move') {
-					var travel = false;
-					var name = connection.name;
-					var room = players[name].room;
-					var target = getTarget(rooms[room], new Line(new Point(players[name].x, players[name].y), new Point(json.x, json.y)));
-					if(rooms[room].zones[0][0].containsPoint(target)) {
-						console.log('Moving ' + name + ' to ' + target);
-						players[name].x = target.x;
-						players[name].y = target.y;
-						players[name].room = players[name].room;
-						for(var i=0; i< rooms[room].zones.length; i++) {
-							var zone = rooms[room].zones[i];
-							if(zone[1] == 'door' && zone[0].containsPoint(target)) {
-								room = travel = zone[2];
-								console.log(name + ' goes to ' + travel);
-								players[name].room = travel;
-								break;
+							console.info('Initial handshake with ' + name);
+							for(var i=0; i < clients.length; i++) {
+								clients[i].sendUTF(JSON.stringify({type: 'enter', name: name, room: players[name].room, x: players[name].x, y: players[name].y, clothing: players[name].clothing}));
+							}
+							connection.sendUTF(JSON.stringify({type: 'syncCloset', closet: players[name].closet}));
+						} else if(json.type == 'move') {
+							var travel = false;
+							var name = connection.name;
+							var room = players[name].room;
+							var target = getTarget(rooms[room], new Line(new Point(players[name].x, players[name].y), new Point(json.x, json.y)));
+							if(rooms[room].zones[0][0].containsPoint(target)) {
+								console.log('Moving ' + name + ' to ' + target);
+								players[name].x = target.x;
+								players[name].y = target.y;
+								players[name].room = players[name].room;
+								for(var i=0; i< rooms[room].zones.length; i++) {
+									var zone = rooms[room].zones[i];
+									if(zone[1] == 'door' && zone[0].containsPoint(target)) {
+										room = travel = zone[2];
+										console.log(name + ' goes to ' + travel);
+										players[name].room = travel;
+										break;
+									}
+								}
+								var msg = {type: 'move', name: name, x: players[name].x, y: players[name].y};
+								if(travel) {
+									msg.travel = travel;
+									players[name].x = msg.newX = rooms[travel].spawn.x;
+									players[name].y = msg.newY = rooms[travel].spawn.y;
+								}
+								for(var i=0; i < clients.length; i++) {
+									clients[i].sendUTF(JSON.stringify(msg));
+								}
+							}
+						} else if(json.type == 'message') {
+							var name = connection.name;
+							console.log(name + ' said ' + json.text);
+							for(var i=0; i < clients.length; i++) {
+								clients[i].sendUTF(JSON.stringify({type: 'say', name: name, text: json.text}));
+							}
+						} else if(json.type == 'addItem') {
+							var name = connection.name;
+							json.itemId = parseInt(json.itemId);
+							if(players[name].closet.indexOf(json.itemId) === -1) {
+								players[name].closet.push(json.itemId);
+								if(dbEnabled) {
+									pgclient.query('update "penguin" set "closet"=$2 where "name"=$1', [name, JSON.stringify(players[name].closet)], function insertPenguinToDb(err) {
+										if(err) {
+											console.log(err);
+										}
+									});
+								}
+							}
+							connection.sendUTF(JSON.stringify({type: 'syncCloset', closet: players[name].closet}));
+							console.log(name + ' acquired ' + json.itemId);
+						} else if(json.type == 'dress') {
+							var name = connection.name;
+							json.itemId = parseInt(json.itemId);
+							console.log(players[name].closet.indexOf(json.itemId), players[name].clothing.indexOf(json.itemId));
+							if(players[name].closet.indexOf(json.itemId) > -1) {
+								if(players[name].clothing.indexOf(json.itemId) > -1) {
+									players[name].clothing.splice(players[name].clothing.indexOf(json.itemId), 1);
+									console.log(name + ' undressed ' + json.itemId);
+								} else {
+									players[name].clothing.push(json.itemId);
+									console.log(name + ' dressed ' + json.itemId);
+								}
+								if(dbEnabled) {
+									pgclient.query('update "penguin" set "clothing"=$2 where "name"=$1', [name, JSON.stringify(players[name].clothing)], function insertPenguinToDb(err) {
+										if(err) {
+											console.log(err);
+										}
+									});
+								}
+								for(var i=0; i < clients.length; i++) {
+									clients[i].sendUTF(JSON.stringify({type: 'dress', name: name, clothing: players[name].clothing}));
+								}
 							}
 						}
-						var msg = {type: 'move', name: name, x: players[name].x, y: players[name].y};
-						if(travel) {
-							msg.travel = travel;
-							players[name].x = msg.newX = rooms[travel].spawn.x;
-							players[name].y = msg.newY = rooms[travel].spawn.y;
-						}
-						for(var i=0; i < clients.length; i++) {
-							clients[i].sendUTF(JSON.stringify(msg));
-						}
+					} catch(ex) {
+						console.error(ex);
 					}
-				} else if(json.type == 'message') {
-					var name = connection.name;
-					console.log(name + ' said ' + json.text);
-					for(var i=0; i < clients.length; i++) {
-						clients[i].sendUTF(JSON.stringify({type: 'say', name: name, text: json.text}));
-					}
-				} else if(json.type == 'addItem') {
-					var name = connection.name;
-					json.itemId = parseInt(json.itemId);
-					if(players[name].closet.indexOf(json.itemId) === -1) {
-						players[name].closet.push(json.itemId);
-					}
-					connection.sendUTF(JSON.stringify({type: 'syncCloset', closet: players[name].closet}));
-					console.log(name + ' acquired ' + json.itemId);
-				} else if(json.type == 'dress') {
-					var name = connection.name;
-					json.itemId = parseInt(json.itemId);
-					console.log(players[name].closet.indexOf(json.itemId), players[name].clothing.indexOf(json.itemId));
-					if(players[name].closet.indexOf(json.itemId) > -1) {
-						if(players[name].clothing.indexOf(json.itemId) > -1) {
-							players[name].clothing.splice(players[name].clothing.indexOf(json.itemId), 1);
-							console.log(name + ' undressed ' + json.itemId);
-						} else {
-							players[name].clothing.push(json.itemId);
-							console.log(name + ' dressed ' + json.itemId);
-						}
-						for(var i=0; i < clients.length; i++) {
-							clients[i].sendUTF(JSON.stringify({type: 'dress', name: name, clothing: players[name].clothing}));
-						}
-					}
+					// connection.sendUTF(message.utf8Data);
 				}
-			} catch(ex) {
-				console.error(ex);
-			}
-			// connection.sendUTF(message.utf8Data);
-		}
-	});
-	connection.on('close', function(reasonCode, description) {
-		var index = clients.indexOf(connection);
-		if(index !== -1) {
-			// remove the connection from the pool
-			clients.splice(index, 1);
-		}
-		registered[connection.name] = players[connection.name];
-		players.removeItem(connection.name);
-		console.log((new Date()) + ' Peer ' + connection.remoteAddress + '(' + connection.name + ') disconnected.');
-		for(var i=0; i < clients.length; i++) {
-			clients[i].sendUTF(JSON.stringify({type: 'exit', name: connection.name}));
-		}
-	});
+			});
+			connection.on('close', function(reasonCode, description) {
+				var index = clients.indexOf(connection);
+				if(index !== -1) {
+					// remove the connection from the pool
+					clients.splice(index, 1);
+				}
+				registered[connection.name] = players[connection.name];
+				players.removeItem(connection.name);
+				console.log((new Date()) + ' Peer ' + connection.remoteAddress + '(' + connection.name + ') disconnected.');
+				for(var i=0; i < clients.length; i++) {
+					clients[i].sendUTF(JSON.stringify({type: 'exit', name: connection.name}));
+				}
+			});
+		});
+	}
 });
