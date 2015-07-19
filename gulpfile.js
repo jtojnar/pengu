64f303f7f3d28053f8181fd2912a7da8ccd28f88
Jan Tojnar <jtojnar@gmail.com>
2015-07-19 23:07:22 +0200

    utilise gulp for asset building

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var autoprefix = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+
+var config = {
+	public: './client'
+}
+
+gulp.task('css', function() {
+	return gulp.src('./assets/client.css')
+		.pipe(csso())
+		.pipe(autoprefix('last 2 versions'))
+		.pipe(gulp.dest(config.public));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./assets/*.css', ['css']);
+});
+
+gulp.task('default', ['css']);
