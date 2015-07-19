ea76767301525ac63e4c81762159bd31028b27e8
Jan Tojnar <jtojnar@gmail.com>
2015-07-20 01:36:12 +0200

    fixed eslint problems

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
 process.title = 'pengu';
-var _dbUri = process.env.DATABASE_URL;
+let _dbUri = process.env.DATABASE_URL;
 
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var express = require('express');
-var pg = require('pg');
-var poly = require('./poly');
-var Point = poly.Point;
-var Polygon = poly.Polygon;
-var Line = poly.Line;
-var pengu = require('./pengu');
-var openid = require('openid');
-var relyingParty = new openid.RelyingParty(
+let WebSocketServer = require('websocket').server;
+let http = require('http');
+let path = require('path');
+let express = require('express');
+let pg = require('pg');
+let poly = require('./poly');
+let Point = poly.Point;
+let Line = poly.Line;
+let pengu = require('./pengu');
+let openid = require('openid');
+let relyingParty = new openid.RelyingParty(
 	process.env.OPENID_VERIFY,
 	process.env.OPENID_REALM, // Realm (optional, specifies realm for OpenID authentication)
 	true, // Use stateless verification
@@ -37,8 +35,8 @@ Object.prototype.removeItem = function(key) {
 };
 
 Array.prototype.getByKey = function(key, value) {
-	var cur = undefined;
-	for(var i = 0, length = this.length; i < length; i++) {
+	let cur;
+	for(let i = 0, length = this.length; i < length; i++) {
 		if(i in this) {
 			cur = this[i];
 			if(cur[key] === value) {
@@ -46,23 +44,23 @@ Array.prototype.getByKey = function(key, value) {
 			}
 		}
 	}
-}
+};
 
 Array.prototype.pushArray = function(arr) {
 	this.push.apply(this, arr);
 };
 
-var app = express();
+let app = express();
 
 app.set('port', process.env.PORT || 8080);
-var serveStatic = require('serve-static');
-app.use('/content', serveStatic(__dirname + '/content'));
-app.use('/client', serveStatic(__dirname + '/client'));
-var session = require('express-session')
+let serveStatic = require('serve-static');
+app.use('/content', serveStatic(path.join(__dirname, 'content')));
+app.use('/client', serveStatic(path.join(__dirname, 'client')));
+let session = require('express-session');
 app.use(session({secret: Math.random().toString(), cookie: { maxAge: 10000 }}));
 
-var env = process.env.NODE_ENV || 'development';
-if(env == 'development') {
+let env = process.env.NODE_ENV || 'development';
+if(env === 'development') {
 	app.use(require('morgan')());
 }
 
@@ -71,18 +69,18 @@ app.get('/', function(req, res) {
 	console.log(req.session.user);
 	if(!req.session.user) {
 		res.redirect('/authenticate');
-	} else if(req.query.u != req.session.user || req.query.g != req.session.group) {
+	} else if(req.query.u !== req.session.user || req.query.g !== req.session.group) {
 		res.redirect('/?u=' + encodeURIComponent(req.session.user) + '&g=' + encodeURIComponent(req.session.group));
 	} else {
 		res.statusCode = 200;
 		res.setHeader('X-User-Name', req.session.user);
 		res.setHeader('X-User-Group', req.session.group);
-		res.sendfile(__dirname + '/client.html');
+		res.sendfile(path.join(__dirname, 'client.html'));
 	}
 });
 
 app.get('/authenticate', function(req, res) {
-	var identifier = process.env.OPENID_PROVIDER;
+	let identifier = process.env.OPENID_PROVIDER;
 
 	// Resolve identifier, associate, and build authentication URL
 	relyingParty.authenticate(identifier, false, function(error, authUrl) {
@@ -112,10 +110,10 @@ app.all('/verify', function(req, res) {
 	});
 });
 
-var server = http.createServer(app).listen(app.get('port'));
+let server = http.createServer(app).listen(app.get('port'));
 console.log('Started app on port %d', app.get('port'));
 
-var wsServer = new WebSocketServer({
+let wsServer = new WebSocketServer({
 	httpServer: server,
 	port: 1337,
 	autoAcceptConnections: false
@@ -130,39 +128,40 @@ function originIsAllowed(origin) {
 // }
 
 function getTarget(room, line) {
-	var gap = 5;
-	var intersections = [];
-	for(var i = room.zones.length - 1; i >= 0; i--) {
-		var zone = room.zones[i];
-		if(zone[1] == 'floor' || zone[1] == 'obstacle') {
+	let gap = 5;
+	let intersections = [];
+	for(let i = room.zones.length - 1; i >= 0; i--) {
+		let zone = room.zones[i];
+		if(zone[1] === 'floor' || zone[1] === 'obstacle') {
 			intersections.pushArray(zone[0].getIntersections(line));
 		}
 	}
+	let target;
 	if(intersections.length > 0){
-		var closest = null, closestDistance;
-		for(var i = intersections.length - 1; i >= 0; i--) {
-			var intersection = intersections[i];
-			var distance = line.start.getDistance(intersection);
-			if(!closest || closestDistance > distance) {
-				closestDistance = distance;
-				closest = intersection;
+		let targetDistance = Infinity;
+		for(let i = intersections.length - 1; i >= 0; i--) {
+			let intersection = intersections[i];
+			let distance = line.start.getDistance(intersection);
+			if(targetDistance > distance) {
+				targetDistance = distance;
+				target = intersection;
 			}
 		}
 	} else {
-		closest = line.end;
+		target = line.end;
 	}
-	closest.x += gap / line.getLength() * (line.start.x - line.end.x);
-	closest.y += gap / line.getLength() * (line.start.y - line.end.y);
+	target.x += gap / line.getLength() * (line.start.x - line.end.x);
+	target.y += gap / line.getLength() * (line.start.y - line.end.y);
 
-	closest.x = Math.round(closest.x);
-	closest.y = Math.round(closest.y);
-	return closest;
+	target.x = Math.round(target.x);
+	target.y = Math.round(target.y);
+	return target;
 }
 
-var clients = [];
-var players = {};
-var rooms = JSON.parse(require('fs').readFileSync(__dirname + '/content/world/map.json', 'utf8'), function (key, value) {
-	var type;
+let clients = [];
+let players = {};
+let rooms = JSON.parse(require('fs').readFileSync(path.join(__dirname, 'content/world/map.json'), 'utf8'), function (key, value) {
+	let type;
 	if(value && typeof value === 'object') {
 		type = value._class;
 		if (typeof type === 'string' && typeof poly[type] === 'function') {
@@ -171,16 +170,16 @@ var rooms = JSON.parse(require('fs').readFileSync(__dirname + '/content/world/ma
 	}
 	return value;
 });
-var items = JSON.parse(require('fs').readFileSync(__dirname + '/content/items/items.json', 'utf8'));
+let items = JSON.parse(require('fs').readFileSync(path.join(__dirname, '/content/items/items.json'), 'utf8'));
 
-var dbEnabled = true;
+let dbEnabled = true;
 pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 	if(err) {
 		pgdone();
 		console.error('Couldn\'t connect to database, data aren\'t persitent', err);
 		dbEnabled = false;
 	}
-	var registered = {};
+	let registered = {};
 	if(dbEnabled) {
 		pgclient.query('select * from "penguin"', getPenguinsFromDb);
 	} else {
@@ -189,7 +188,7 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 	function getPenguinsFromDb(err, result) {
 		if(!err) {
 			if(result) {
-				for(var i = 0; i < result.rows.length; i++) {
+				for(let i = 0; i < result.rows.length; i++) {
 					registered[result.rows[i].name] = result.rows[i];
 				}
 			}
@@ -197,7 +196,7 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 			console.error('Couldn\'nt read penguins from db', err);
 			dbEnabled = false;
 		}
-	
+
 		wsServer.on('request', function(request) {
 			if (!originIsAllowed(request.origin)) {
 				request.reject();
@@ -205,15 +204,15 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 				return;
 			}
 
-			var connection = request.accept(null, request.origin);
+			let connection = request.accept(null, request.origin);
 			clients.push(connection);
 			console.log((new Date()) + ' Connection accepted.');
 			connection.on('message', function(message) {
 				if(message.type === 'utf8') {
 					try {
-						var json = JSON.parse(message.utf8Data);
+						let json = JSON.parse(message.utf8Data);
 						console.log(json);
-						if(json.type == 'init' && connection.name == null) {
+						if(json.type === 'init' && connection.name === undefined) {
 							if(!json.name || json.name.trim() === '') {
 								connection.drop(pengu.NO_USERNAME_PROVIDED, 'No username provided');
 								return;
@@ -222,10 +221,10 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 								connection.drop(pengu.USERNAME_ALREADY_BEING_USED, 'Username already being used');
 								return;
 							}
-							var name = connection.name = json.name;
+							let name = connection.name = json.name;
 							connection.sendUTF(JSON.stringify({type: 'sync', name: name, data: players}));
 							if(!registered.hasOwnProperty(name)) {
-								registered[name] = {clothing: [], closet: [], registered: (new Date).toUTCString(), group: json.group};
+								registered[name] = {clothing: [], closet: [], registered: (new Date()).toUTCString(), group: json.group};
 								if(dbEnabled) {
 									pgclient.query('insert into "penguin"("name", "closet", "clothing", "registered", "group") VALUES($1, $2, $3, $4, $5)', [name, JSON.stringify(registered[name].closet), JSON.stringify(registered[name].clothing), registered[name].registered, registered[name].group], function insertPenguinToDb(err) {
 										if(err) {
@@ -240,46 +239,46 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 							players[name].room = 'plaza';
 
 							console.info('Initial handshake with ' + name);
-							for(var i=0; i < clients.length; i++) {
+							for(let i = 0; i < clients.length; i++) {
 								clients[i].sendUTF(JSON.stringify({type: 'enter', name: name, room: players[name].room, x: players[name].x, y: players[name].y, clothing: players[name].clothing}));
 							}
 							connection.sendUTF(JSON.stringify({type: 'syncCloset', closet: players[name].closet}));
-						} else if(json.type == 'move') {
-							var travel = false;
-							var name = connection.name;
-							var room = players[name].room;
-							var target = getTarget(rooms[room], new Line(new Point(players[name].x, players[name].y), new Point(json.x, json.y)));
+						} else if(json.type === 'move') {
+							let travel = false;
+							let name = connection.name;
+							let room = players[name].room;
+							let target = getTarget(rooms[room], new Line(new Point(players[name].x, players[name].y), new Point(json.x, json.y)));
 							if(rooms[room].zones[0][0].containsPoint(target)) {
 								console.log('Moving ' + name + ' to ' + target);
 								players[name].x = target.x;
 								players[name].y = target.y;
 								players[name].room = players[name].room;
-								for(var i=0; i< rooms[room].zones.length; i++) {
-									var zone = rooms[room].zones[i];
-									if(zone[1] == 'door' && zone[0].containsPoint(target)) {
+								for(let i = 0; i < rooms[room].zones.length; i++) {
+									let zone = rooms[room].zones[i];
+									if(zone[1] === 'door' && zone[0].containsPoint(target)) {
 										room = travel = zone[2];
 										console.log(name + ' goes to ' + travel);
 										players[name].room = travel;
 										break;
 									}
 								}
-								var msg = {type: 'move', name: name, x: players[name].x, y: players[name].y};
+								let msg = {type: 'move', name: name, x: players[name].x, y: players[name].y};
 								if(travel) {
 									msg.travel = travel;
 									players[name].x = msg.newX = rooms[travel].spawn.x;
 									players[name].y = msg.newY = rooms[travel].spawn.y;
 								}
-								for(var i=0; i < clients.length; i++) {
+								for(let i = 0; i < clients.length; i++) {
 									clients[i].sendUTF(JSON.stringify(msg));
 								}
 							}
-						} else if(json.type == 'message') {
+						} else if(json.type === 'message') {
 							json.text = json.text.trim();
 							if(json.text !== '') {
-								var name = connection.name;
+								let name = connection.name;
 								if(json.text.indexOf('/') === 0) {
 									if(json.text.indexOf('/mute ') === 0 && ['admin', 'moderator'].indexOf(players[name].group) > -1) {
-										var bannedName = json.text.substr(6);
+										let bannedName = json.text.substr(6);
 										players[bannedName].banned = true;
 										if(dbEnabled) {
 											pgclient.query('update "penguin" set "banned"=true where "name"=$1', [bannedName], function mutePenguin(err) {
@@ -289,7 +288,7 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 											});
 										}
 									} else if(json.text.indexOf('/unmute ') === 0 && ['admin', 'moderator'].indexOf(players[name].group) > -1) {
-										var bannedName = json.text.substr(8);
+										let bannedName = json.text.substr(8);
 										players[bannedName].banned = false;
 										if(dbEnabled) {
 											pgclient.query('update "penguin" set "banned"=false where "name"=$1', [bannedName], function mutePenguin(err) {
@@ -302,7 +301,7 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 								} else {
 									if(!players[name].banned) {
 										console.log(name + ' said ' + json.text);
-										for(var i=0; i < clients.length; i++) {
+										for(let i = 0; i < clients.length; i++) {
 											clients[i].sendUTF(JSON.stringify({type: 'say', name: name, text: json.text}));
 										}
 									} else {
@@ -310,8 +309,8 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 									}
 								}
 							}
-						} else if(json.type == 'addItem') {
-							var name = connection.name;
+						} else if(json.type === 'addItem') {
+							let name = connection.name;
 							json.itemId = parseInt(json.itemId);
 							if(!items.getByKey('id', json.itemId).available) {
 								connection.sendUTF(JSON.stringify({type: 'error', message: 'Tato věc nejde v současnosti získat.'}));
@@ -331,8 +330,8 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 								connection.sendUTF(JSON.stringify({type: 'error', message: 'Tuto věc již máš.'}));
 								console.log(name + ' attempted to reacquire ' + json.itemId);
 							}
-						} else if(json.type == 'dress') {
-							var name = connection.name;
+						} else if(json.type === 'dress') {
+							let name = connection.name;
 							json.itemId = parseInt(json.itemId);
 							if(players[name].closet.indexOf(json.itemId) > -1) {
 								if(players[name].clothing.indexOf(json.itemId) > -1) {
@@ -349,7 +348,7 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 										}
 									});
 								}
-								for(var i=0; i < clients.length; i++) {
+								for(let i = 0; i < clients.length; i++) {
 									clients[i].sendUTF(JSON.stringify({type: 'dress', name: name, clothing: players[name].clothing}));
 								}
 							}
@@ -361,7 +360,7 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 				}
 			});
 			connection.on('close', function(reasonCode, description) {
-				var index = clients.indexOf(connection);
+				let index = clients.indexOf(connection);
 				if(index !== -1) {
 					// remove the connection from the pool
 					clients.splice(index, 1);
@@ -369,7 +368,7 @@ pg.connect(_dbUri, function connectToDb(err, pgclient, pgdone) {
 				registered[connection.name] = players[connection.name];
 				players.removeItem(connection.name);
 				console.log((new Date()) + ' Peer ' + connection.remoteAddress + '(' + connection.name + ') disconnected.' + (description ? ' Reason: ' + description : ''));
-				for(var i=0; i < clients.length; i++) {
+				for(let i = 0; i < clients.length; i++) {
 					clients[i].sendUTF(JSON.stringify({type: 'exit', name: connection.name}));
 				}
 			});
