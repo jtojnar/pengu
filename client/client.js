d32691c699ba99c6d70ea3a68ba5eb699a1df02f
Jan Tojnar <jtojnar@gmail.com>
2015-07-20 02:04:28 +0200

    client code eslint fixes

--- client/client.js
+++ client/client.js
@@ -1,30 +1,32 @@
-"use strict";
+'use strict';
+/* eslint-env browser, jquery */
+
 function getParameterByName(name) {
-	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+	var regexS = '[\\?&]' + name + '=([^&#]*)';
 	var regex = new RegExp(regexS);
 	var results = regex.exec(window.location.search);
-	if(results == null) {
-		return "";
+	if (results == null) {
+		return '';
 	} else {
-		return decodeURIComponent(results[1].replace(/\+/g, " "));
+		return decodeURIComponent(results[1].replace(/\+/g, ' '));
 	}
 }
 
 function removeItemNamed(object, key) {
-	if(!object.hasOwnProperty(key)){
+	if (!object.hasOwnProperty(key)){
 		return;
 	}
-	if(isNaN(parseInt(key)) || !(object instanceof Array)) {
+	if (isNaN(parseInt(key)) || !(object instanceof Array)) {
 		delete object[key];
 	} else {
 		object.splice(key, 1);
 	}
-};
+}
 
 function findById(arr, id) {
-	for(var i = 0; i < arr.length; i++) {
-		if(arr[i].id === id) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].id === id) {
 			return arr[i];
 		}
 	}
@@ -32,7 +34,7 @@ function findById(arr, id) {
 }
 
 $(function () {
-	if(getParameterByName('u').trim() === '') {
+	if (getParameterByName('u').trim() === '') {
 		window.location.href = '/';
 	}
 
@@ -53,7 +55,7 @@ $(function () {
 
 	var log = $('<div class="log"><a href="#" class="close">×</a><div class="log-inner"></div></div>');
 	view.append(log);
-	$('.log-inner').vertiscroll({ width:5, color:'#f07', footer: 0 });
+	$('.log-inner').vertiscroll({ width: 5, color: '#f07', footer: 0 });
 	log.hide();
 	$('.log').click(function stopPropagation(e) {
 		e.preventDefault();
@@ -90,7 +92,7 @@ $(function () {
 
 	$('#toggleMusic').click(function toggleMusic(e) {
 		audio.get(0).muted = !audio.get(0).muted;
-		if(audio.get(0).muted) {
+		if (audio.get(0).muted) {
 			$(this).find('img').attr('src', '/client/unmute-button.png');
 		} else {
 			$(this).find('img').attr('src', '/client/mute-button.png');
@@ -106,12 +108,12 @@ $(function () {
 
 	window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-	if(!window.WebSocket) {
+	if (!window.WebSocket) {
 		view.html('Smůla, tvůj prohlížeč nepodporuje WebSockets.');
 		return;
 	}
 
-	var connection = new WebSocket('ws://'+window.location.hostname + (window.location.port == '' ? '' : ':' + window.location.port));
+	var connection = new WebSocket('ws://' + window.location.hostname + (window.location.port === '' ? '' : ':' + window.location.port));
 
 	connection.onopen = function () {
 		connection.send(JSON.stringify({type: 'init', name: getParameterByName('u'), group: getParameterByName('g')}));
@@ -124,17 +126,17 @@ $(function () {
 		$.when(mapLoaded, itemsLoaded).done(function() {
 			loadRoom();
 		}).fail(function() {
-			alert('Načítání selhalo. Zkus prosím obnovit stránku.')
+			alert('Načítání selhalo. Zkus prosím obnovit stránku.');
 		});
 	};
 
 	connection.onclose = function(e) {
 		clearInterval(monitor);
 		alert(e.reason);
-		if(e.code === 4001 || e.code === 4002) {
+		if (e.code === 4001 || e.code === 4002) {
 			window.location.href = '/';
 		}
-	}
+	};
 
 	connection.onerror = function (error) {
 		view.html('<p>Promiň, nefunguje server nebo tvé připojení.</p>');
@@ -147,55 +149,55 @@ $(function () {
 		// the massage is not chunked or otherwise damaged.
 		try {
 			var json = JSON.parse(message.data);
-		} catch(e) {
+		} catch (e) {
 			console.log('This doesn\'t look like a valid JSON: ', message.data);
 			return;
 		}
 		console.log(json);
-		if(json.type === 'sync') {
+		if (json.type === 'sync') {
 			myName = json.name;
-			for(var key in json.data) {
-				if(json.data.hasOwnProperty(key)){
+			for (var key in json.data) {
+				if (json.data.hasOwnProperty(key)){
 					addPlayer(key, json.data[key].room, json.data[key].x, json.data[key].y);
 					itemsLoaded.done(function() {
 						dressPlayer(key, json.data[key].clothing);
 					});
 				}
 			}
-		} else if(json.type === 'enter') {
+		} else if (json.type === 'enter') {
 			addPlayer(json.name, json.room, json.x, json.y);
 			itemsLoaded.done(function() {
 				dressPlayer(json.name, json.clothing);
 			});
-		} else if(json.type === 'exit') {
+		} else if (json.type === 'exit') {
 			removePlayer(json.name);
-		} else if(json.type === 'move') {
-			if(json.travel) {
+		} else if (json.type === 'move') {
+			if (json.travel) {
 				movePlayer(json.name, json.x, json.y, json.travel, json.newX, json.newY);
 			} else {
 				movePlayer(json.name, json.x, json.y);
 			}
-		} else if(json.type === 'say') {
+		} else if (json.type === 'say') {
 			var scrollDown = false;
-			if(($('.log-inner').scrollTop() + $('.log-inner').height()) === $('.log-inner').get(0).scrollHeight) {
+			if (($('.log-inner').scrollTop() + $('.log-inner').height()) === $('.log-inner').get(0).scrollHeight) {
 				scrollDown = true;
 			}
 			$('.log-inner').append($('<p><strong>' + json.name + '</strong> </p>').append(document.createTextNode(json.text)));
-			if(scrollDown) {
+			if (scrollDown) {
 				$('.log-inner').scrollTop($('.log-inner').get(0).scrollHeight);
 			}
 
 			speakForPlayer(json.name, json.text);
-		} else if(json.type === 'dress') {
+		} else if (json.type === 'dress') {
 			itemsLoaded.done(function() {
 				dressPlayer(json.name, json.clothing);
 			});
-		} else if(json.type === 'syncCloset') {
+		} else if (json.type === 'syncCloset') {
 			itemsLoaded.done(function() {
 				myCloset = json.closet;
 				fillInventory();
 			});
-		} else if(json.type === 'error') {
+		} else if (json.type === 'error') {
 			alert(json.message);
 		} else {
 			console.log('Hmm..., I\'ve never seen JSON like this: ', json);
@@ -207,25 +209,25 @@ $(function () {
 	});
 
 	$('#message').keydown(function(e) {
-		if(e.keyCode == 13 && $(this).val().trim() !== ''){
+		if (e.keyCode === 13 && $(this).val().trim() !== ''){
 			connection.send(JSON.stringify({type: 'message', text: $(this).val().trim()}));
 			$(this).val('');
 		}
 	});
 
 	var monitor = setInterval(function() {
-		if(connection.readyState !== 1) {
+		if (connection.readyState !== 1) {
 			clearInterval(monitor);
 			alert('Nelze komunikovat s WebSocket serverem.');
 		}
 	}, 3000);
 
 	function addPlayer(name, room, x, y) {
-		players[name] = $('<div data-name="'+name+'" data-room="'+room+'" class="penguin"><div class="message"><p>I am a penguin eating zebra filled with bubblegum</p></div></div>');
-		if(typeof x !== 'undefined') {
+		players[name] = $('<div data-name="' + name + '" data-room="' + room + '" class="penguin"><div class="message"><p>I am a penguin eating zebra filled with bubblegum</p></div></div>');
+		if (typeof x !== 'undefined') {
 			changePlayerPosition(name, x, y);
 		}
-		if(room == myRoom) {
+		if (room === myRoom) {
 			showPlayer(name);
 		}
 	}
@@ -233,22 +235,22 @@ $(function () {
 		view.append(players[name]);
 	}
 	function movePlayer(name, x, y, room, newX, newY) {
-		// if(16 <= x && x <= view.width() - 16 && 16 <= y && y <= view.height() - 16)
+		// if (16 <= x && x <= view.width() - 16 && 16 <= y && y <= view.height() - 16)
 		var left = players[name].position().left;
 		var top = players[name].position().top;
 		var distance = Math.sqrt(Math.pow(left - x, 2) + Math.pow(top - y, 2));
 
 		var handler = function() {};
-		if(typeof room !== 'undefined') {
+		if (typeof room !== 'undefined') {
 			players[name].attr('data-room', room);
-			if(name == myName) {
+			if (name === myName) {
 				handler = function() {
 					myRoom = room;
 					loadRoom();
 					changePlayerPosition(myName, newX, newY);
-					for(var key in players) {
-						if(players.hasOwnProperty(key)){
-							if(players[key].attr('data-room') == myRoom) {
+					for (var key in players) {
+						if (players.hasOwnProperty(key)){
+							if (players[key].attr('data-room') === myRoom) {
 								showPlayer(key);
 							} else {
 								hidePlayer(key);
@@ -257,7 +259,7 @@ $(function () {
 					}
 				};
 			} else {
-				if(room == myRoom) {
+				if (room === myRoom) {
 					handler = false;
 					changePlayerPosition(name, newX, newY);
 					showPlayer(name);
@@ -269,8 +271,8 @@ $(function () {
 				}
 			}
 		}
-		if(handler !== false) {
-			players[name].animate({top: y, left: x}, distance / speed * 1000, 'linear',  handler);
+		if (handler !== false) {
+			players[name].animate({top: y, left: x}, distance / speed * 1000, 'linear', handler);
 		}
 	}
 	function removePlayer(name) {
@@ -281,7 +283,7 @@ $(function () {
 		players[name].remove();
 	}
 	function speakForPlayer(name, message) {
-		if(msgTimeouts[name]) {
+		if (msgTimeouts[name]) {
 			clearTimeout(msgTimeouts[name]);
 		}
 		msgTimeouts[name] = setTimeout(function() {
@@ -295,34 +297,34 @@ $(function () {
 	function dressPlayer(name, clothing) {
 		players[name].find('img.clothing').remove();
 		console.log(clothing);
-		for(var i = 0; i < clothing.length; i++) {
+		for (var i = 0; i < clothing.length; i++) {
 			console.log(findById(items, clothing[i]).file);
 			players[name].append($('<img src="/content/items/' + findById(items, clothing[i]).file + '" class="clothing">'));
 		}
 	}
 	function fillInventory() {
 		$('.inventory-inner img').remove();
-		for(var i = 0; i < myCloset.length; i++) {
+		for (var i = 0; i < myCloset.length; i++) {
 			var itemData = findById(items, myCloset[i]);
 			var item = $('<img src="/content/items/paper/' + itemData.id + '.png" width="50" height="50" alt="" title="' + itemData.title + '" data-item="' + itemData.id + '">');
 			$('.inventory-inner').append(item);
-		};
-		$('.inventory-inner').vertiscroll({ width:5, color:'#f07' });
+		}
+		$('.inventory-inner').vertiscroll({ width: 5, color: '#f07' });
 	}
 
 	function loadRoom() {
 		var promises = [];
 		overlay.show();
 		audio.get(0).pause();
-		if(typeof map[myRoom].ambiance !== 'undefined') {
+		if (typeof map[myRoom].ambiance !== 'undefined') {
 			audio.attr('src', '/content/world/' + map[myRoom].ambiance);
 			audio.get(0).play();
 		}
-		for(var layer in myLayers) {
+		for (var layer in myLayers) {
 			myLayers[layer].remove();
 		}
 		myLayers = [];
-		for(var layerdata in map[myRoom].layers) {
+		for (var layerdata in map[myRoom].layers) {
 			var layerdata = map[myRoom].layers[layerdata];
 			var layer = $('<img class="layer">');
 			layer.css({
@@ -340,35 +342,35 @@ $(function () {
 
 			layer.attr('src', '/content/world/' + layerdata.file);
 
-			if(layerdata.alternate) {
+			if (layerdata.alternate) {
 				layer.hide();
 			}
 
-			if(layerdata.item) {
+			if (layerdata.item) {
 				layer.addClass('item');
 				layer.attr('data-item', layerdata.item);
 				layer.click(function itemClicked(e) {
 					var itemId = $(this).attr('data-item');
-					if(confirm('Opravdu chceš získat ' + findById(items, parseInt(itemId)).title)) {
+					if (confirm('Opravdu chceš získat ' + findById(items, parseInt(itemId)).title)) {
 						connection.send(JSON.stringify({type: 'addItem', itemId: itemId}));
 					}
 					e.stopPropagation();
-				})
+				});
 			}
 
 			myLayers.push(layer);
 			view.append(layer);
 		}
 
-		if(getParameterByName('debug') == 1) {
+		if (getParameterByName('debug') == 1) {
 			ctx.lineWidth = 3;
 			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-			for(var zonedata in map[myRoom].zones) {
+			for (var zonedata in map[myRoom].zones) {
 				var zonedata = map[myRoom].zones[zonedata];
 				ctx.beginPath();
 				ctx.strokeStyle = {floor: '#a6d924', door: '#a924d9', obstacle: '#d92463', sound: '#0075ff', animate: '#ffd200'}[zonedata[1]];
 				ctx.moveTo(zonedata[0].points[0].x, zonedata[0].points[0].y);
-				for(var i = 0; i < zonedata[0].points.length; i++) {
+				for (var i = 0; i < zonedata[0].points.length; i++) {
 					ctx.lineTo(zonedata[0].points[i].x, zonedata[0].points[i].y);
 				}
 				ctx.lineTo(zonedata[0].points[0].x, zonedata[0].points[0].y);
@@ -376,11 +378,11 @@ $(function () {
 				ctx.closePath();
 			}
 			ctx.beginPath();
-			ctx.strokeStyle = '#06d2d4'
-			ctx.moveTo(map[myRoom].spawn.x, map[myRoom].spawn.y-6);
-			ctx.lineTo(map[myRoom].spawn.x + Math.sqrt(45), map[myRoom].spawn.y+3);
-			ctx.lineTo(map[myRoom].spawn.x - Math.sqrt(45), map[myRoom].spawn.y+3);
-			ctx.lineTo(map[myRoom].spawn.x, map[myRoom].spawn.y-6);
+			ctx.strokeStyle = '#06d2d4';
+			ctx.moveTo(map[myRoom].spawn.x, map[myRoom].spawn.y - 6);
+			ctx.lineTo(map[myRoom].spawn.x + Math.sqrt(45), map[myRoom].spawn.y + 3);
+			ctx.lineTo(map[myRoom].spawn.x - Math.sqrt(45), map[myRoom].spawn.y + 3);
+			ctx.lineTo(map[myRoom].spawn.x, map[myRoom].spawn.y - 6);
 			ctx.stroke();
 		}
 
