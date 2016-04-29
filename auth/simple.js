bf01d988428c678e950ac90d59a4d91883a5879e
Jan Tojnar <jtojnar@gmail.com>
2016-04-29 02:03:41 +0200

    autofocus simple auth nickname field

--- auth/simple.js
+++ auth/simple.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
 			res.send(`
 				<meta charset="utf-8">
 				<form action="" method="get">
-				<label>Přezdívka: <input type="text" name="name"></label>
+				<label>Přezdívka: <input type="text" name="name" autofocus></label>
 				<button type="submit">Přihlásit se</button>
 				</form>
 			`);
