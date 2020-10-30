abf45427157b06408f15f7bd29da39ac7fa027ff
Jan Tojnar <jtojnar@gmail.com>
2020-10-30 22:12:47 +0100

    add Nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Lightweight online chat inspired by Club Penguin";
+
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-compat, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nodejs_latest
+          ];
+        };
+      }
+    );
+}
